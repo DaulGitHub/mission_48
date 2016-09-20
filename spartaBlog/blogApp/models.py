d822bb006c279e7b3d3f36d2aab08cc718b3e830
Daulet Mavlonazarov <dmavlonazarov@arello-mobile.com>
2016-09-20 12:20:51 +0700

    Сделал регистрацию юзера

--- spartaBlog/blogApp/models.py
+++ spartaBlog/blogApp/models.py
@@ -1,5 +1,8 @@
-from __future__ import unicode_literals
+# coding: utf8
+from django.contrib.auth.models import User
 
-from django.db import models
-
-# Create your models here.
+# Модифицируем поле email.
+# _meta это экземпляр django.db.models.options.Options, который хранит данные о модели.
+# Это немного хак, но я пока не нашел более простого способа переопределить поле из базовой модели.
+User._meta.get_field('email')._unique = True
+User._meta.get_field('email').blank = False
--- spartaBlog/blogApp/urls.py
+++ spartaBlog/blogApp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^registration$', views.RegisterFormView.as_view()),
+    url(r'^login$', views.LoginFormView.as_view()),
+    url(r'^logout$', views.LogoutView.as_view()),
 ]
