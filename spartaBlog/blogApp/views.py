787423c10977a90fbc451b4fa1b8afc98200158b
Daulet Mavlonazarov <dmavlonazarov@arello-mobile.com>
2016-09-29 16:13:13 +0700

    Сделал пагиницию страниц

--- spartaBlog/blogApp/views.py
+++ spartaBlog/blogApp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from forms import AddCommentForm
 from models import Comment
+from django.core.paginator import Paginator
 
 
 @login_required
@@ -23,6 +24,14 @@ def comments(request):
         for comment in all_comments:
             prepare_comments.append({"user_name": comment.user_name, "email": comment.email, "message": comment.message})
 
-    context = {"form": add_form, "comments": prepare_comments}
+    current_page_number = 1
+    if request.GET.get("page"):
+        current_page_number = int(request.GET.get("page"))
+    comments_on_page = Paginator(prepare_comments, 3)
+    current_page_comments = comments_on_page.page(current_page_number)
+
+    context = {"form": add_form, "comments": current_page_comments,
+               "next_page": current_page_comments.next_page_number() if current_page_comments.has_next() else current_page_number,
+               "previous_page": current_page_comments.previous_page_number() if current_page_comments.has_previous() else current_page_number}
 
     return render(request, template_name='blog/comments.html', context=context)
